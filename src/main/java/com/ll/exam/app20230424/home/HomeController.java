4d2757fbf949bd0f2c3920e6f702726cbd4c6e2d
Choi-Hyeok99 <gur0709@naver.com>
2023-04-24 20:20:45 +0900

    세팅

---
+++ src/main/java/com/ll/exam/app20230424/home/HomeController.java
@@ -0,0 +1,17 @@
+package com.ll.exam.app20230424.home;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String showMain(){
+        return "Hello world";
+
+    }
+}
